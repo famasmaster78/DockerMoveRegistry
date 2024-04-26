f7d30edc4e76eb831a5edc24b0e64dd9add372d3
jbgaard <jxras11@gmail.com>
2024-04-26 12:18:32 +0200

    Add project files.

---
+++ DockerMoveRegistry/Program.cs
@@ -0,0 +1,320 @@
+﻿
+// Import docker registry class
+DockerMoveRegistry.DockerAPI? oldDocker = null;
+DockerMoveRegistry.DockerAPI? newDocker = null;
+
+// Start message
+Console.WriteLine("Starting DockerMoveRegistry");
+
+// Tell what this program does
+Console.WriteLine("This program moves a Docker registry from one server to another.");
+
+// Check if DockerAPI is defined, otherwise prompt user to define it
+void CheckDockerAPI()
+{
+    if (oldDocker == null)
+    {
+        Console.WriteLine("Please define the old Docker registry API");
+        Console.Write("Enter the URL: ");
+        string oldURL = Console.ReadLine();
+        oldDocker = new DockerMoveRegistry.DockerAPI(oldURL);
+
+        // Do you want to set credentials?
+        Console.Write("Do you want to set credentials? (y/n): ");
+
+        string input = Console.ReadLine();
+
+        // If yes, set credentials
+        if (input == "y")
+        {
+
+            // Credentials already set?
+            bool credentialsSet = false;
+
+            // Check if credentials are already set on newDocker
+            if (newDocker != null && newDocker.client.DefaultRequestHeaders.Authorization != null)
+            {
+
+                // Do you want to use the same credentials as newDocker?
+                Console.Write("Do you want to use the same credentials as new Docker Registry? (y/n): ");
+
+                string input2 = Console.ReadLine();
+
+                if (input2 == "y")
+                {
+                    oldDocker.client.DefaultRequestHeaders.Authorization = newDocker.client.DefaultRequestHeaders.Authorization;
+                    credentialsSet = true;
+                }
+
+            }
+
+            // If credentials are not set, set them
+            if (!credentialsSet)
+            {
+                (string username, string password) = GetCredentials();
+                oldDocker.SetCredentials(username, password);
+            }
+        }
+
+    }
+    if (newDocker == null)
+    {
+        Console.WriteLine("Please define the new Docker registry API");
+        Console.Write("Enter the URL: ");
+        string newURL = Console.ReadLine();
+        newDocker = new DockerMoveRegistry.DockerAPI(newURL);
+
+        // Do you want to set credentials?
+        Console.Write("Do you want to set credentials? (y/n): ");
+
+        string input = Console.ReadLine();
+
+        // If yes, set credentials
+        if (input == "y")
+        {
+
+            // Credentials already set?
+            bool credentialsSet = false;
+
+            // Check if credentials are already set on oldDocker
+            if (oldDocker != null && oldDocker.client.DefaultRequestHeaders.Authorization != null)
+            {
+
+                // Do you want to use the same credentials as oldDocker?
+                Console.Write("Do you want to use the same credentials as new Docker Registry? (y/n): ");
+
+                string input2 = Console.ReadLine();
+
+                if (input2 == "y")
+                {
+                    newDocker.client.DefaultRequestHeaders.Authorization = oldDocker.client.DefaultRequestHeaders.Authorization;
+                    credentialsSet = true;
+                }
+
+            }
+
+            // If credentials are not set, set them
+            if (!credentialsSet)
+            {
+                (string username, string password) = GetCredentials();
+                newDocker.SetCredentials(username, password);
+            }
+        }
+    }
+
+    // Log docker api
+    Console.WriteLine("Old Docker registry: " + oldDocker.client.BaseAddress);
+    Console.WriteLine("New Docker registry: " + newDocker.client.BaseAddress);
+}
+
+// Get credentials
+(string,string) GetCredentials()
+{
+    Console.Write("Enter the username: ");
+    string username = Console.ReadLine();
+    Console.Write("Enter the password: ");
+    string password = Console.ReadLine();
+    return (username, password);
+}
+
+// Function to select action
+void SelectAction()
+{
+
+    // If DockerAPI are defined, print the URL
+    if (oldDocker != null && newDocker != null)
+    {
+        Console.WriteLine("Old Docker registry: " + oldDocker.client.BaseAddress);
+        Console.WriteLine("New Docker registry: " + newDocker.client.BaseAddress);
+    }
+
+    // Show numbered list of possibilities
+    Console.WriteLine("What do you want to do?");
+    Console.WriteLine("1. Move a registry");
+    Console.WriteLine("2. Pull all repositories");
+    Console.WriteLine("3. List all repositories");
+    Console.WriteLine("4. List all tags for a repository");
+    Console.WriteLine("-------------------------------");
+    Console.WriteLine("6. Exit");
+
+    // Get user input
+    Console.Write("Enter a number: ");
+    string input = Console.ReadLine();
+
+    // Check user input
+    if (input == "1")
+    {
+        // Move a registry
+        Console.WriteLine("Move a registry");
+    }
+    else if (input == "2")
+    {
+        // Pull all repositories
+        Console.WriteLine("Pull all repositories");
+
+        // Check if DockerAPI is defined
+        CheckDockerAPI();
+
+        // Get all repositories for each registry
+        List<string> oldRepos = oldDocker.GetRepositories().Result;
+        List<string> newRepos = newDocker.GetRepositories().Result;
+
+        // Pull all repositories from old to new registry
+        foreach (string repo in oldRepos)
+        {
+            // Pull all tags for the repository
+            List<string> tags = oldDocker.GetTags(repo).Result;
+
+            // Pull all tags from old to new registry
+            foreach (string tag in tags)
+            {
+                Console.WriteLine("Pulling " + repo + ":" + tag);
+                // Pull image
+                // Run command: $"docker pull {oldDocker.client.BaseAddress}/{repo}:{tag}"
+
+            }
+        }
+    }
+    else if (input == "3")
+    {
+        // Check if DockerAPI is defined
+        CheckDockerAPI();
+
+        // Get all repositories for each registry
+        List<string> oldRepos = oldDocker.GetRepositories().Result;
+        List<string> newRepos = newDocker.GetRepositories().Result;
+
+        // Show repositories
+        Console.WriteLine("Old registry repositories:");
+        foreach (string repo in oldRepos)
+        {
+            Console.WriteLine(repo);
+        }
+
+        Console.WriteLine();
+
+        Console.WriteLine("New registry repositories:");
+        foreach (string repo in newRepos)
+        {
+            Console.WriteLine(repo);
+        }
+
+        // Press any key to continue
+        Console.WriteLine("Press any key to continue");
+    }
+    else if (input == "4")
+    {
+        // New or old registry?
+        Console.Write("Do you want to list tags for the old or new registry? (old/new): ");
+        string registry = Console.ReadLine();
+
+        // Check if DockerAPI is defined
+        CheckDockerAPI();
+
+        // Check if old or new registry
+        if (registry == "old")
+        {
+            // Get all repositories for old registry
+            List<string> oldRepos = oldDocker.GetRepositories().Result;
+
+            // Show repositories with numering
+            Console.WriteLine("Old registry repositories:");
+            for (int i = 0; i < oldRepos.Count; i++)
+            {
+                Console.WriteLine(i + ". " + oldRepos[i]);
+            }
+
+            // Select repository
+            Console.Write("Enter the repository: ");
+            string repositoryNumber = Console.ReadLine();
+
+            // Get repository name
+            string repository = oldRepos[int.Parse(repositoryNumber)];
+
+            Console.WriteLine(repository);
+
+            // Get all tags for the repository
+            List<string> tags = oldDocker.GetTags(repository).Result;
+
+            // Show tags
+            Console.WriteLine("Tags for " + repository + ":");
+            foreach (string tag in tags)
+            {
+                Console.WriteLine(tag);
+            }
+        }
+        else if (registry == "new")
+        {
+            // Get all repositories for new registry
+            List<string> newRepos = newDocker.GetRepositories().Result;
+
+            // Show repositories with numbering
+            Console.WriteLine("New registry repositories:");
+            for (int i = 0; i < newRepos.Count; i++)
+            {
+                Console.WriteLine(i + ". " + newRepos[i]);
+            }
+
+            // Select repository
+            Console.Write("Enter the repository: ");
+            string repositoryNumber = Console.ReadLine();
+
+            // Get repository name
+            string repository = newRepos[int.Parse(repositoryNumber)];
+
+            Console.WriteLine(repository);
+
+            // Get all tags for the repository
+            List<string> tags = newDocker.GetTags(repository).Result;
+
+            // Show tags
+            Console.WriteLine("Tags for " + repository + ":");
+            foreach (string tag in tags)
+            {
+                Console.WriteLine(tag);
+            }
+        }else
+        {
+            // Invalid input
+            Console.WriteLine("Invalid input");
+        }
+
+    }
+    else if (input == "6")
+    {
+        // Exit
+        Console.WriteLine("Exiting DockerMoveRegistry");
+        return;
+    }
+    else
+    {
+        // Invalid input
+        Console.WriteLine("Invalid input");
+        // Press any key to continue
+        Console.WriteLine("Press any key to continue");
+        Console.ReadKey();
+        // Clear console
+        Console.Clear();
+        // Show menu again
+        SelectAction();
+        return;
+    }
+
+    // Press any key to continue
+    Console.WriteLine("Press any key to continue");
+    Console.ReadKey();
+
+    // Clear console
+    Console.Clear();
+    SelectAction();
+}
+
+// Show menu
+SelectAction();
+
+// End message
+Console.WriteLine("DockerMoveRegistry ended");
+
+// Read key to exit
+Console.ReadKey();
+
---
+++ DockerRegistryAPI/DockerRegistryAPI.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace DockerRegistryAPI
+{
+    public class API
+    {
+
+        // HTTP Client
+        public HttpClient client;
+
+        // Constructor
+        public API(string URL)
+        {
+            client = new HttpClient();
+            client.BaseAddress = new Uri(URL);
+        }
+
+        // Set login credentials
+        public void SetCredentials(string username, string password)
+        {
+            string credentials = Convert.ToBase64String(Encoding.ASCII.GetBytes(username + ":" + password));
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", credentials);
+        }
+
+        // Get all repositories
+        public async Task<List<string>> GetRepositories()
+        {
+            List<string> repositories = new List<string>();
+            HttpResponseMessage response = await client.GetAsync("/v2/_catalog");
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                JObject json = JObject.Parse(content);
+                JArray repos = (JArray)json["repositories"];
+                foreach (string repo in repos)
+                {
+                    repositories.Add(repo);
+                }
+            }
+            return repositories;
+        }
+
+        // Get all tags for a repository
+        public async Task<List<string>> GetTags(string repository)
+        {
+            List<string> tags = new List<string>();
+            HttpResponseMessage response = await client.GetAsync("/v2/" + repository + "/tags/list");
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                JObject json = JObject.Parse(content);
+                JArray tagsArray = (JArray)json["tags"];
+                foreach (string tag in tagsArray)
+                {
+                    tags.Add(tag);
+                }
+            }
+            return tags;
+        }
+
+    }
+}
